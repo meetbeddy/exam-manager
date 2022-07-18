9eb1568e1d50a4e2cd9e3aadefce5a8e0d144c20
meetbeddy <katlybedrick@gmail.com>
2022-07-18 12:15:44 +0100

    add correct fonts

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -9,7 +9,16 @@ function Dashboard(props) {
       <NavBar />
       <div className="main-layout">
         <Aside />
-        <div className="main-content">{props.children}</div>
+        <div className="main-content">
+          {props.children}
+
+          <p className="footer">
+            Powered By{" "}
+            <span>
+              <a href="#educenty">Educenty</a>
+            </span>
+          </p>
+        </div>
       </div>
     </DashboardContainer>
   );
--- src/components/dashboard/dashboardStyles.js
+++ src/components/dashboard/dashboardStyles.js
@@ -3,8 +3,9 @@ import styled from "styled-components";
 export const DashboardContainer = styled.div`
   height: 100%;
   width: 100%;
+  position: relative;
   margin: 0 0 20px;
-  overflow: hidden;
+  // overflow: hidden;
   .main-layout {
     width: 100%;
     display: flex;
@@ -13,13 +14,39 @@ export const DashboardContainer = styled.div`
 
     .main-content {
       width: 80%;
+
+      position: relative;
+      .footer {
+        position: absolute;
+        height: 18px;
+        top: 96%;
+        left: 86%;
+        right: 0;
+        bottom: 100;
+
+        font-family: Roboto;
+        font-size: 15px;
+        font-style: italic;
+        font-weight: 300;
+        line-height: 18px;
+        letter-spacing: 0em;
+        text-align: left;
+
+        span {
+          font-weight: 600;
+
+          a {
+            color: inherit;
+            text-decoration: underline;
+          }
+        }
+      }
     }
   }
 `;
 
 export const AsideContainer = styled.div`
   display: flex;
-
   width: 20%;
   text-align: center;
   .menu {
